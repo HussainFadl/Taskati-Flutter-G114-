ee30fcde5338b286b6d43a4f716e1babb63e3726
HussainFadl <96234529+HussainFadl@users.noreply.github.com>
2024-02-03 17:49:50 +0200

    The ListView in HomeView Design

--- lib/Featuers/home_view.dart
+++ lib/Featuers/home_view.dart
@@ -1,5 +1,4 @@
-
-
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
@@ -17,35 +16,43 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
+    return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           elevation: 25,
           backgroundColor: AppColors.greyColor,
-          title:Text('I am AppBar',
-          style: getTitleStyle(),),
-          centerTitle: true,),
-        body:  Padding(
+          title: Text(
+            'I am AppBar',
+            style: getTitleStyle(),
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
           padding: const EdgeInsets.all(10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // header
               Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Hello, Hussain',
-                      style: getTitleStyle(color: AppColors.primaryColor),),
-                      Text('Have a nice day',
-                      style: getSmallStyle(color: AppColors.blackColor),),
+                      Text(
+                        'Hello, Hussain',
+                        style: getTitleStyle(color: AppColors.primaryColor),
+                      ),
+                      Text(
+                        'Have a nice day',
+                        style: getSmallStyle(color: AppColors.blackColor),
+                      ),
                     ],
                   ),
                   const Spacer(),
                   const CircleAvatar(
                     backgroundImage: AssetImage('Assets/accountingImage.png'),
-                    maxRadius:25  ,
+                    maxRadius: 25,
                   )
                 ],
               ),
@@ -53,24 +60,107 @@ class _HomeViewState extends State<HomeView> {
               Row(
                 children: [
                   Text(
-                    style: getTitleStyle(),
-                    DateFormat.yMMMd().format(DateTime.now(),
-                    )
-                  ),
-                  
-                  Spacer(),
-                  CustomButton(text: '+ Add Task', onPressed: () {
-                    
-                  },)
+                      style: getTitleStyle(),
+                      DateFormat.yMMMd().format(
+                        DateTime.now(),
+                      )),
+                  const Spacer(),
+                  SizedBox(
+                    height: 50,
+                    child: CustomButton(
+                      text: '+ Add Task',
+                      onPressed: () {},
+                    ),
+                  )
                 ],
               ),
               Text(
-                    'Today',
-                    style: getSmallStyle(),
-                   )
-                  ,
-          
-          
+                'Today',
+                style: getTitleStyle(),
+              ),
+              Gap(20),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  DatePicker(
+                    DateTime.now(),
+                    initialSelectedDate: DateTime.now(),
+                    selectionColor: AppColors.primaryColor,
+                    selectedTextColor: Colors.white,
+                    onDateChange: (date) {
+                      // New date selected
+                      // setState(() {
+                      //   _selectedValue = date;
+                      // });
+                    },
+                    height: 114,
+                    width: 75,
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(itemCount: 3,itemBuilder: (context, index){
+                   return  Container(
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.circular(15),
+
+                    ),
+                    child:  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              Text('data',
+                              style: TextStyle(
+                                color: AppColors.whiteColor,
+                              ),
+                              ),
+                              Row(
+                                children: [
+                                  Icon(Icons.watch_later_outlined,
+                                  color: AppColors.whiteColor,),
+                                  Text('data',
+                                  style: TextStyle(
+                                    color: AppColors.whiteColor
+                                  ),),
+                                ],
+                              ),
+                              Text('data',
+                              style: TextStyle(
+                                color: AppColors.whiteColor
+                              )),
+                            ],
+                          ),
+                          Spacer(),
+                          Container(
+                            height: 70,
+                            width: 0.5,
+                            decoration: BoxDecoration(
+                              color: AppColors.whiteColor,
+                              shape: BoxShape.rectangle,
+                            ),
+                          ),
+                          Gap(10),
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Text('TO DO',
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: AppColors.whiteColor
+                            ),),
+                          ),
+                        ],
+                      ),
+                    ),
+                    
+                  );
+                }),
+              )
             ],
           ),
         ),
